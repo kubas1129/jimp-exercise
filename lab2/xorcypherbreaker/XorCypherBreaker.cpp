09fa4ca0d8f262d2272a9103612db94fc58747cb
JP <kubas1129@fedora.kubas1129>
2017-03-14 18:43:45 +0100

    Add file for XorCypherBreaker and update from origin

---
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -0,0 +1,10 @@
+//
+// Created by kubas1129 on 14.03.17.
+//
+
+#include "XorCypherBreaker.h"
+
+std::string XorCypherBreaker(const std::vector<char> &cryptogram, int key_length, const std::vector<string> &dictionary)
+{
+    return "";
+}
