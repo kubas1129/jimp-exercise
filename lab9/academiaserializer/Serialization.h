3af1451fe72b9ff48bbaa44f8f63c6be4e3120d8
JP <kubas1129@fedora.kubas1129>
2017-05-08 14:54:48 +0200

    Add solution for XmlSerializer

--- lab9/academiaserializer/Serialization.h
+++ lab9/academiaserializer/Serialization.h
@@ -34,11 +34,53 @@ namespace academia {
         std::ostream *out_serializator;
     };
 
+    class JsonSerializer{
+    public:
+
+        JsonSerializer(std::ostream *out) : out_serializator(out) {}
+
+        virtual void IntegerField(const std::string &field_name, int value);
+        virtual void DoubleField(const std::string &field_name, double value);
+        virtual void StringField(const std::string &field_name, const std::string &value);
+        virtual void BooleanField(const std::string &field_name, bool value) {}
+        virtual void SerializableField(const std::string &field_name, const academia::Serializable &value) {}
+        virtual void ArrayField(const std::string &field_name, const vector<reference_wrapper<const academia::Serializable>> &value) {}
+        virtual void Header(const std::string &object_name);
+        virtual void Footer(const std::string &object_name);
+
+
+        virtual void AddToOutput(std::string in) { (*out_serializator) << in; }
+        virtual void AddCommaToOutput() { (*out_serializator) << std::string(", "); }
+
+    protected:
+        std::ostream *out_serializator;
+    };
+
+    class XmlSerializer{
+    public:
+
+        XmlSerializer(std::ostream *out) : out_serializator(out) {}
+
+        virtual void IntegerField(const std::string &field_name, int value);
+        virtual void DoubleField(const std::string &field_name, double value);
+        virtual void StringField(const std::string &field_name, const std::string &value);
+        virtual void BooleanField(const std::string &field_name, bool value) {}
+        virtual void SerializableField(const std::string &field_name, const academia::Serializable &value) {}
+        virtual void ArrayField(const std::string &field_name, const vector<reference_wrapper<const academia::Serializable>> &value) {}
+        virtual void Header(const std::string &object_name);
+        virtual void Footer(const std::string &object_name);
+
+    protected:
+        std::ostream *out_serializator;
+
+    };
+
 
     class Serializable{
     public:
         virtual void Serialize(Serializer *serializer) =0;
-
+        virtual void Serialize(JsonSerializer *jsonserializer) =0;
+        virtual void Serialize(XmlSerializer *xmlserializer) =0;
     };
 
     class Room : public Serializable{
@@ -46,18 +88,20 @@ namespace academia {
 
         enum Type{
             COMPUTER_LAB,
-            OTHER_LAB,
-            SOME_LAB,
+            LECTURE_HALL,
+            CLASSROOM,
         };
+
+        Room(int id, std::string room_number, Type typ): id_(id), room_number_(room_number), typ_(typ) {}
 
         std::string EnumToString(Type type) {
             switch(type) {
                 case Type::COMPUTER_LAB:
-                    return "first lab";
-                case Type::OTHER_LAB:
-                    return "other lab";
-                case Type::SOME_LAB:
-                    return "some lab";
+                    return "COMPUTER_LAB";
+                case Type::LECTURE_HALL:
+                    return "LECTURE_HALL";
+                case Type::CLASSROOM:
+                    return "CLASSROOM";
             }
         }
 
@@ -69,12 +113,80 @@ namespace academia {
             serializer->Footer("Room");
         }
 
-        Room(int id, std::string room_number, Type typ): id_(id), room_number_(room_number), typ_(typ) {}
+        void Serialize(JsonSerializer *jsonserializer) override {
+            jsonserializer->Header("{");
+            jsonserializer->IntegerField("id", id_);
+            jsonserializer->AddCommaToOutput();
+            jsonserializer->StringField("name", room_number_);
+            jsonserializer->AddCommaToOutput();
+            jsonserializer->StringField("type", EnumToString(typ_));
+            jsonserializer->Footer("}");
+        }
+
+        void Serialize(XmlSerializer *xmlserializer) override {
+            xmlserializer->Header("room");
+            xmlserializer->IntegerField("id", id_);
+            xmlserializer->StringField("name", room_number_);
+            xmlserializer->StringField("type", EnumToString(typ_));
+            xmlserializer->Footer("room");
+        }
 
     private:
         int id_;
         std::string room_number_;
         Type typ_;
+    };
+
+
+    class Building : public Serializable{
+    public:
+        Building(int id, std::string name, std::initializer_list<Room> rooms): id_(id), name_(name) {
+            for(const auto &v : rooms)
+            {
+                rooms_.push_back(v);
+            }
+        }
+
+
+        void Serialize(Serializer *serializer) override {}
+
+        void Serialize(JsonSerializer* jsonserializer) override {
+            jsonserializer->Header("{");
+            jsonserializer->IntegerField("id", id_);
+            jsonserializer->AddCommaToOutput();
+            jsonserializer->StringField("name", name_);
+            jsonserializer->AddCommaToOutput();
+            jsonserializer->AddToOutput("\""s + "rooms" + "\": "s);
+            jsonserializer->Header("[");
+            int leng = 0;
+            for(auto &v : rooms_)
+            {
+                leng++;
+                v.Serialize(jsonserializer);
+                if(leng < rooms_.size()) jsonserializer->AddToOutput(", ");
+            }
+            jsonserializer->Footer("]");
+            jsonserializer->Footer("}");
+        }
+
+        void Serialize(XmlSerializer *xmlserializer) override {
+            xmlserializer->Header("building");
+            xmlserializer->IntegerField("id", id_);
+            xmlserializer->StringField("name", name_);
+            xmlserializer->Header("rooms");
+            for(auto &v : rooms_)
+            {
+                v.Serialize(xmlserializer);
+            }
+            xmlserializer->Footer("rooms");
+            xmlserializer->Footer("building");
+        }
+
+    private:
+        int id_;
+        std::string name_;
+        std::vector<Room> rooms_;
+
     };
 
 
